67140d86eb06536e94f7e72daeb9b02c32d82f40
caralinebruz <caraline.bruzinski@gmail.com>
2022-10-14 16:34:36 -0400

    initial parse of input file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import sys, getopt
 import argparse
 
+import pprint
+from pprint import pprint
 
 ''' This takes an input graph file and a maximum number of colors to use
 	and solves the colormap problem such that no adjacent nodes can share
@@ -9,10 +11,66 @@ import argparse
 
 	USAGE: ./main.py [-v] $ncolors $input-file
 '''
-
 num_colors = ""
 
 
+class Node:
+	def __init__(self,name):
+		self.name = name
+		self.value = None
+		self.children = []
+		self.parents = []
+
+	def add_child(self, child):
+		child.parents.append(self)
+		self.children.append(child)
+
+
+# store all the node objects by their node name
+# could have used a tree data structure for this but alas, i didnot. 
+created_node_objects = {}
+nodes_list = []
+node_adjacency_mappings = {}
+
+
+
+def parse_input(lines):
+	''' Go through the file initially
+			save names and values of things etc.
+	'''
+	lines = [line.rstrip('\n') for line in lines]
+
+	for line in lines:
+		# we know it is a list of child nodes
+		data = line.split(':')
+
+		# 1. save the name of the node
+		node_name = data[0].strip(' ')
+		if node_name not in nodes_list:
+			nodes_list.append(node_name)
+
+		# 2. add the children of this to the node_adjacency_mappings
+		children = data[1].lstrip()
+		children = children.strip('[] ').split(', ')
+
+		node_adjacency_mappings[node_name] = children
+
+		# check to also add leaf nodes
+		for i in children:
+			if i not in nodes_list:
+				nodes_list.append(i)
+
+
+	print("names of all nodes in the graph:")
+	print(nodes_list)
+	print("node-adjacency-mappings:")
+	pprint(node_adjacency_mappings)
+
+	return nodes_list, node_adjacency_mappings
+
+
+def build_adjacency(nodes_list,node_adjacency_mappings):
+	pass
 
 
 if __name__ == '__main__':
@@ -42,7 +100,14 @@ if __name__ == '__main__':
 		infile = args.graph_file
 
 
-	# # First, set up the game
+	# First, parse the input graph file
+	nodes_list, node_adjacency_mappings = parse_input(lines)
+	# create the adjacency matrix
+	build_adjacency(nodes_list, node_adjacency_mappings)
+
+	# tree_node = setup.build_tree(nodes_list, leaf_values, node_adjacency_mappings)
+
+
 	# tree = game_setup(lines)
 	# # Second, play the game
 	# game_play(tree, role)
